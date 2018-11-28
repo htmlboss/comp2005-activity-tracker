b83af40e302f9655ad1ec0f5be4931a822384d9e
Jacob House <jacob@jacobhouse.ca>
2018-11-28 15:19:41 -0330

    Generated docs for new methods

--- docs/classcom_1_1activitytracker_1_1_run.js
+++ docs/classcom_1_1activitytracker_1_1_run.js
@@ -2,6 +2,7 @@ var classcom_1_1activitytracker_1_1_run =
 [
     [ "Run", "classcom_1_1activitytracker_1_1_run.html#a5568c1c514835056d2abc22cfba222c5", null ],
     [ "bulkImport", "classcom_1_1activitytracker_1_1_run.html#a8e2b13e0096b87614d5333ec15213300", null ],
+    [ "getID", "classcom_1_1activitytracker_1_1_run.html#a61916c14ab5a2bf6b080200f7d0c5566", null ],
     [ "getRuns", "classcom_1_1activitytracker_1_1_run.html#a1aa1fb01eabff586e16d88f19f7df743", null ],
     [ "newRunDataPoint", "classcom_1_1activitytracker_1_1_run.html#a5dea6f1860431103d553ce770382afe0", null ],
     [ "altitude_ascended", "classcom_1_1activitytracker_1_1_run.html#a247fe29ef5dceeb25750fc7c2701f5c8", null ],
@@ -10,5 +11,6 @@ var classcom_1_1activitytracker_1_1_run =
     [ "date", "classcom_1_1activitytracker_1_1_run.html#a66934b1f4fe6bc74a4e98574a2892764", null ],
     [ "dbManager", "classcom_1_1activitytracker_1_1_run.html#ab90e32eda9f4c671ae3575f971edca6b", null ],
     [ "distance", "classcom_1_1activitytracker_1_1_run.html#a7b4ca8c4ecea4da1653f03b8c8fc16a8", null ],
-    [ "duration", "classcom_1_1activitytracker_1_1_run.html#a5e38d293d29d4b65c9290ff4bee82e03", null ]
+    [ "duration", "classcom_1_1activitytracker_1_1_run.html#a5e38d293d29d4b65c9290ff4bee82e03", null ],
+    [ "id", "classcom_1_1activitytracker_1_1_run.html#aa76717aee690b5bfe919d6e87dea1d84", null ]
 ];
--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['newrun',['newRun',['../classcom_1_1activitytracker_1_1_d_b_manager.html#a05b742f583167f6ce00eb8415c43fc1c',1,'com::activitytracker::DBManager']]],
+  ['newrun',['newRun',['../classcom_1_1activitytracker_1_1_d_b_manager.html#a25beffc49198126ce078e5ebb473b695',1,'com::activitytracker::DBManager']]],
   ['newrundatapoint',['newRunDataPoint',['../classcom_1_1activitytracker_1_1_run.html#a5dea6f1860431103d553ce770382afe0',1,'com::activitytracker::Run']]]
 ];
